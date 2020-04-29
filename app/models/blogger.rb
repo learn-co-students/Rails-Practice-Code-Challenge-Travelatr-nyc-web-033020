65130a83ff5d7fe4e581d53325aa85b9ed99feef
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-04-29 17:43:59 -0400

    Done.

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,26 @@
 class Blogger < ApplicationRecord
+   has_many :posts 
+
+   validates :name, uniqueness: true
+   validates :age, numericality: { minimum: 1 }
+   validates :bio, length: { minimum: 30 }
+   
+   def total_num_likes 
+      Post.all.select do |post|
+         post.blogger_id == self.id 
+      end.map{ |post| post.likes }.count 
+   end 
+
+   def my_posts 
+      Post.all.select do |post|
+         post.blogger_id == self.id 
+      end
+   end 
+
+   def most_liked_post 
+      self.my_posts.select do |post|
+         post.likes 
+      end.max 
+   end 
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,10 @@
 class Post < ApplicationRecord
+    belongs_to :blogger 
+    belongs_to :destination 
+
+
+    def like
+        self.likes += 1 
+    end
 
 end
